e43f55705a1f90153a5209983fe30d5280f513a2
Methni0616 <methnimanarandi7@gmail.com>
2025-08-25 14:21:57 +0530

    Add delete and edit buttons

--- script.js
+++ script.js
@@ -16,11 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
-        const li = document.createElement('li');
+       const li = document.createElement('li');
         li.innerHTML = `
-            <input type="checkbox" class="task-checkbox">
-            
+            <input type="checkbox" class="checkbox">
             <span>${taskText}</span>
+            <div class="task-buttons">
+                <button class="edit-btn"><i class="fa-solid fa-pen"></i></button>
+                <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
+            </div>
         `;
 
         taskList.appendChild(li);
